57e7411ad21612dd9231f71b0c06f15efe293906
Kishi Aeraki <sluggyben@icloud.com>
2021-02-17 22:16:25 -0500

    AES X

--- index.js
+++ index.js
@@ -7,18 +7,55 @@ var currentsave = {
 		[
 
 		]
-	]
+	],
+	custom_themes: [
+		/*{
+			name: 'Test Theme',
+			base: 'aesx',
+			font: 'Default',
+			style: ''
+		}*/
+	],
+	title: 'AES',
+	data_version: 1
 };
+
+var themedictionary = {
+	'AES X': 'aesx',
+	'MATERIAL': 'mtrl',
+	'AES Classic': 'aesc',
+	'Retro': 'rtro',
+	'Vaporwave': 'vapo',
+	'SPICE ORANGE': 'spor',
+	'Persona 5': 'psn5',
+	'Persona 3': 'psn3',
+	'K-ON!': 'konb'
+};
+var fontdictionary = {
+	'Default': 0,
+	'Arial': 'Arial, Helvetica, sans-serif',
+	'Monospace': "'Courier New', monospace",
+	'Cursive': "'Brush Script MT', cursive",
+	'Comic Sans': '"Comic Sans MS", cursive, sans-serif'
+};
+
+var custom_theme_default = `body {
+	background-color: darkred;
+	color: cyan;
+}
+`;
 
 function renderLinks() {
 	$('#shortcuts').html('');
-	$('#links .linklist').html('');
-	for (let n=0; n<2; n++) {
-		for (let i=0; i<currentsave.links[n].length; i++) {
+	$('#menu_links .linklist').html('');
+	$('#HEADER').text(currentsave.title);
+	$('#menu_other #title_input').val(currentsave.title);
+	for (let n=0; n<2; n++) { // For each row.
+		for (let i=0; i<currentsave.links[n].length; i++) { // For each link in the row
 			
 			let l = currentsave.links[n][i];
 
-			// Shortcuts
+			// Create Link Boxes
 			let d = $('<div>');
 			d.append( $('<span>').text(l.name) );
 			d.addClass('link');
@@ -29,25 +66,26 @@ function renderLinks() {
 			};
 			$('#shortcuts').append(d);
 
-			// Links Menu
+			// Update Links Menu
 			let a = $('<div>').addClass('label');
 			a.append( $('<b>').text('X').addClass('del').click( function() {
 				currentsave.links[n].splice(i, 1); renderLinks();
 				saveSettings();
 			}));
 			a.append( $('<b>').text('✎').addClass('edt').click( function() {
+				// Open Link Edit Menu
 				openMenu('linkedit');
-				$('#linkedit #label').val(l.name);
-				$('#linkedit #url').val(l.url);
-
-				$('#linkedit .topbutton').click( function () {
-					currentsave.links[n][i].name = $('#linkedit #label').val();
-					currentsave.links[n][i].url  = $('#linkedit #url').val();
+				$('#menu_linkedit #label').val(l.name);
+				$('#menu_linkedit #url').val(l.url);
+				// Set 'Save' Button to Enact Changes on CLick
+				$('#menu_linkedit .topbutton').click( function () {
+					currentsave.links[n][i].name = $('#menu_linkedit #label').val();
+					currentsave.links[n][i].url  = $('#menu_linkedit #url').val();
+					saveSettings();
 					renderLinks();
-					$('#linkedit .topbutton').off();
 					openMenu('links');
+					$('#menu_linkedit .topbutton').off(); // Removes function after being used.
 				});
-				saveSettings();
 
 			}));
 			a.append( $('<b>').text('⬇').addClass('mov').click( function() {
@@ -84,7 +122,6 @@ function renderLinks() {
 			}));
 			a.append( $('<div>').text(l.name) );
 			$('#row' + (n+1) + 'linklist').append(a);
-
 		};
 		$('#shortcuts').append( $('<br>') );
 	};
@@ -98,26 +135,122 @@ function toggleMenu() {
 
 function openMenu(id) {
 	$('.menupage').hide();
-	$('#'+id).show();
+	$('#menu_'+id).show();
 };
 
 function setTheme(theme) {
+	// Wash Theming
 	$('body').removeClass();
-	$('body').addClass(theme);
+	$('#customtheme').empty();
+
+	// Add Theming
+	if (theme.startsWith('custom_')) {
+		let ctheme = currentsave.custom_themes[parseInt(theme.substring(7))];
+		$('body').addClass(ctheme.base);
+		$('#customtheme').text('body { font-family: '+
+		fontdictionary[ctheme.font]+' !important;}\n'+ctheme.style);
+	} else {
+		$('body').addClass(theme);
+	};
 	currentsave.theme = theme;
 	saveSettings();
 };
 
-function saveSettings() {
-	localStorage.setItem('saveslot', JSON.stringify(currentsave));
-	//$('#savebutton').css('background-color', '#59e03e');
-	//setTimeout( function() { $('#savebutton').css('background-color', '#eee'); }, 200);
+function renderCustomThemeMenu() {
+	$('#customthemelist').empty();
+	for (let i=0; i<currentsave.custom_themes.length; i++) {
+		let ct = currentsave.custom_themes[i];
+		let d = $('<div>').addClass('label');
+		$('#customthemelist').append(d);
+
+		// Buttons
+		d.append( $('<b>').text('X').addClass('del').click( function() {
+			currentsave.custom_themes.splice(i, 1);
+			renderCustomThemeMenu();
+			setTheme('aesx');
+		}));
+		d.append( $('<b>').text('✎').addClass('edt').click( function() {
+
+			setTheme('custom_'+i);
+
+			// Render Editor
+			$('#menu_customtheme #name').val(ct.name);
+			$('#menu_customtheme #base').val(ct.base);
+			$('#menu_customtheme #font').val(ct.font);
+			csseditor.setValue(ct.style);
+
+			// Open Editor
+			openMenu('customtheme');
+
+			// On Save & Exit
+			$('#menu_customtheme .topbutton').click( function () {
+				saveCustomTheme(i);
+				openMenu('themes');
+				$('#menu_linkedit .topbutton').off(); // Removes function after being used.
+				$('#menu_linkedit #savechanges').off();
+			});
+			$('#menu_customtheme #savechanges').click( function() {
+				saveCustomTheme(i);
+			});
+		}));
+		d.append( $('<div>').text(ct.name).addClass('theme_name').click( function() {
+			setTheme('custom_'+i);
+		}));
+	};
 };
+
+function saveCustomTheme(array_index) {
+	//ADD SAVE FEATURE
+	let themepacket = {
+		name: $('#menu_customtheme #name').val(),
+		base: $('#menu_customtheme #base').val(),
+		font: $('#menu_customtheme #font').val(),
+		style: csseditor.getValue()
+	};
+	currentsave.custom_themes[array_index] = themepacket;
+	renderCustomThemeMenu();
+	setTheme('custom_'+array_index);
+	saveSettings();
+};
+
+function addCustomTheme() {
+	currentsave.custom_themes.push({
+		name: 'New Theme',
+		base: 'aesx',
+		font: 'Default',
+		style: custom_theme_default
+	});
+	renderCustomThemeMenu();
+	saveSettings();
+};
+
+function resetSettings() {
+	let confirmation = prompt('Resetting AESLink will remove all links and Custom Themes. \
+	Type `Reset` to confirm the reset.');
+	if (confirmation === 'Reset') {
+		localStorage.clear();
+		location.reload();
+	} else {
+		alert('Reset Aborted');
+	};
+};
+
+function saveSettings() { localStorage.setItem('saveslot', JSON.stringify(currentsave)) };
 
 function loadSettings() {
 	if ( localStorage.getItem('saveslot') !== null ) {
+		// Load and Update LocalStorage Settings
 		currentsave = JSON.parse(localStorage.getItem('saveslot'));
+		if (currentsave.data_version === undefined) {
+			currentsave.data_version = 1;
+			currentsave.title = 'AES';
+			currentsave.custom_themes = [];
+			saveSettings();
+		};
+
+
 		setTheme(currentsave.theme); 
 	};
 	renderLinks();
+	renderCustomThemeMenu();
 };
